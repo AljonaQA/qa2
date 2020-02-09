1c42b4599eb17df50e6c180721f6c5df769982c4
AljonaQA <zest@inbox.lv>
2020-02-09 13:27:54 +0300

    tmp

---
+++ src/test/java/OurFirstTest.java
@@ -0,0 +1,9 @@
+import org.junit.jupiter.api.Test;
+
+public class OurFirstTest {
+    @Test
+    public void firsttest() {
+        System.out.println("Hello World");
+        System.out.println();
+    }
+}
